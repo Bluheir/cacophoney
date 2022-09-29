78f9c2f98c6fef665b16325afd1e2778e1182c8a
Bluheir <slimcognitodudesvids@gmail.com>
2022-09-29 18:47:45 +1000

    Created file.rs for file helper methods

---
+++ src/helpers/file.rs
@@ -0,0 +1,65 @@
+use std::error::Error;
+
+use aes_gcm::{
+    aead::{Aead, Nonce},
+    Aes256Gcm, KeyInit,
+};
+use generic_array::GenericArray;
+use serde::{de::DeserializeOwned, Serialize};
+use tokio::{
+    fs::File,
+    io::{AsyncReadExt, AsyncWriteExt},
+};
+
+pub async fn get_toml<T: DeserializeOwned>(
+    path: &str,
+    default: &'static str,
+) -> Result<T, Box<dyn Error>> {
+    let config = File::open(path).await;
+
+    let mut f = match config {
+        Ok(f) => f,
+        Err(_) => {
+            log::info!("Creating TOML file at location {}", path);
+            let mut f = File::create(path).await?;
+
+            // Write default config
+            f.write_all(default.as_bytes()).await?;
+
+            let obj = toml::from_str::<T>(default)?;
+            return Ok(obj);
+        }
+    };
+
+    let mut v = String::new();
+    f.read_to_string(&mut v).await?;
+
+    Ok(toml::from_str::<T>(&v)?)
+}
+
+pub async fn read_encrypted<T: DeserializeOwned>(
+    pass: &[u8; 32],
+    nonce: &[u8; 12],
+    cipher: &[u8],
+) -> Result<T, Box<dyn Error>> {
+    let pass = GenericArray::from_slice(pass);
+    let nonce = GenericArray::from_slice(nonce);
+
+    let d = Aes256Gcm::new(pass);
+
+    let dec = d.decrypt(nonce, cipher)?;
+    Ok(serde_cbor::from_slice(&dec)?)
+}
+pub async fn encrypt<T: Serialize>(
+    pass: &[u8; 32],
+    nonce: &[u8; 12],
+    data: &T,
+) -> Result<Vec<u8>, Box<dyn Error>> {
+    let pass = GenericArray::from_slice(pass);
+    let nonce = GenericArray::from_slice(nonce);
+
+    let d = Aes256Gcm::new(pass);
+    let p = serde_cbor::to_vec(data)?;
+
+    Ok(d.encrypt(nonce, &*p)?)
+}
