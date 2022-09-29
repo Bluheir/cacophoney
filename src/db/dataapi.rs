a22c4ecd2240236c65c18786bcda208d0e868f94
Bluheir <slimcognitodudesvids@gmail.com>
2022-09-29 18:46:46 +1000

    Added subaccounts function

--- src/db/dataapi.rs
+++ src/db/dataapi.rs
@@ -1,4 +1,17 @@
-pub trait DbApi {}
-pub struct Empty {}
+use async_trait::async_trait;
 
-impl DbApi for Empty {}
+use crate::data::{crypto::PubKey, SubAccount};
+
+#[async_trait]
+pub trait DbApi {
+    // TODO add error type
+    async fn get_subaccounts(&mut self, key : &PubKey) -> Result<Vec<SubAccount>, ()>;
+}
+pub struct EmptyDB {}
+
+#[async_trait]
+impl DbApi for EmptyDB {
+    async fn get_subaccounts(&mut self, _key : &PubKey) -> Result<Vec<SubAccount>, ()> {
+        Err(())
+    }
+}
