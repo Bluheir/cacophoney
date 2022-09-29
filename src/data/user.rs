1d92f95f5af027ae6f4a672e30f41ca9f73f40a5
Bluheir <slimcognitodudesvids@gmail.com>
2022-09-29 18:46:13 +1000

    Changed types

--- src/data/user.rs
+++ src/data/user.rs
@@ -2,15 +2,24 @@ use serde::{Deserialize, Serialize};
 
 use super::crypto::PubKey;
 
-#[derive(Serialize, Deserialize, Clone)]
 /// Represents a User who can send messages to other clients or could store other messages
+#[derive(Serialize, Deserialize, Clone)]
 pub struct User {
-    /// My "Master" nickname
+    /// Master nickname of the user
     pub nickname: String,
-    /// My public key
+    /// Public key of the user
     pub pub_key: PubKey,
 }
+/// Represents an account that receives messages
+#[derive(Serialize, Deserialize, Clone, Copy)]
 pub struct SubAccount {
     /// My public key
     pub pub_key: PubKey,
+    pub publicity : Publicity
+}
+
+#[derive(Serialize, Deserialize, Clone, Copy, PartialEq, Eq)]
+pub enum Publicity {
+    Public,
+    Private
 }
