56782248bb8b64f7d2c2e51c24133b093c178b8b
Bluheir <slimcognitodudesvids@gmail.com>
2022-10-03 10:29:55 +1100

    Changed main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![feature(let_chains)]
 
-use aes_gcm::aead::Aead;
+use aes_gcm::Key;
 use aes_gcm::{aead::OsRng, Aes256Gcm};
-use aes_gcm::{Key, KeyInit};
-
-use futures::io::Cursor;
+use log::LevelFilter;
+use quinn::ServerConfig;
 use rand::RngCore;
+use server::start_empty;
+use std::io::Cursor;
+use std::sync::Arc;
 use std::{error::Error, time::Duration};
 use tokio::sync::mpsc::{self, Receiver, Sender};
 use tokio::time::sleep;
@@ -13,13 +15,17 @@ use tokio::{
     fs::File,
     io::{AsyncReadExt, AsyncWriteExt},
 };
+use tui::style::Style;
 use ui::{ActionExecution, InputChangeReq, SelectOption};
+use uilog::Logger;
 
 use generic_array::GenericArray;
 
 use crate::config::{Configuration, SecretConfiguration};
+use crate::db::EmptyDb;
 use crate::helpers::file::{encrypt, read_encrypted};
 use crate::helpers::hash_s;
+use crate::server::NodeService;
 
 pub mod config;
 pub mod data;
@@ -27,6 +33,9 @@ pub mod db;
 pub mod helpers;
 pub mod server;
 pub mod ui;
+pub mod uilog;
+
+static mut LOGGER: Option<Logger> = None;
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -34,9 +43,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let (s_send, t_recv) = mpsc::channel::<InputChangeReq>(1);
     // The terminal thread sends data to the main async tokio task
     let (t_send, s_recv) = mpsc::channel::<ActionExecution>(1);
+    // Logging send and receive
+    let (log_send, log_recv) = mpsc::channel::<Vec<(String, Style)>>(1);
+
+    unsafe {
+        LOGGER = Some(Logger::new(log_send));
+        match &LOGGER {
+            Some(t) => log::set_logger(t).map(|()| log::set_max_level(LevelFilter::Info)),
+            None => panic!(),
+        }
+        .unwrap();
+    }
 
     std::thread::spawn(move || {
-        let _ = ui::terminal_init(t_send, t_recv);
+        let _ = ui::terminal_init(t_send, t_recv, log_recv);
     });
 
     let mut listener = TerminalListener {
@@ -47,13 +67,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Wait until the terminal GUI is ready
     sleep(Duration::from_millis(10)).await;
 
+    log::error!("wake up");
+
     let config = get_config().await?;
     let secrets = File::open(&config.secret_config.location).await;
 
     let secrets = match secrets {
         Ok(mut f) => {
-            log::debug!("Opening secrets file");
-
             let mut cipher = Vec::new();
             f.read_to_end(&mut cipher).await?;
 
@@ -61,7 +81,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
             let mut dec_config = None;
 
-            match config.secret_config.password {
+            match &config.secret_config.password {
                 Some(p) => {
                     let h = hash_s(&p);
                     dec_config = read_encrypted::<SecretConfiguration>(&h, &nonce, &cipher)
@@ -76,12 +96,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 // Start requesting the password
                 for i in 0..4 {
                     let pass = listener
-                        .pass_hash(
-                            format!(
-                                "Please type in the password to unlock the secrets file. ({}/4)",
-                                i + 1
-                            ),
-                        )
+                        .pass_hash(format!(
+                            "Please type in the password to unlock the secrets file. ({}/4)",
+                            i + 1
+                        ))
                         .await?;
 
                     dec_config = read_encrypted(&pass, &nonce, &cipher).await.ok();
@@ -145,7 +163,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
                 // Get or generate the nonce
                 let nonce = get_nonce().await?;
-
                 // Encrypt
                 let w = encrypt(&pass, &nonce, &secret).await?;
 
@@ -160,16 +177,64 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     };
 
-    if config.main_config.secure &&
-        let Some(path) = config.main_config.cert_path &&
-        let Some(private_path) = config.main_config.private_key_path {
+    let mut cert =
 
-        if let Ok(mut pubfile) = File::open(path).await &&
-            let Ok(mut privfile) = File::open(private_path).await {
+    if let Ok(mut pubfile) = File::open(&config.main_config.cert_path).await &&
+        let Ok(mut privfile) = File::open(&config.main_config.private_key_path).await {
+        // Public key reading
+        let mut buf = String::new();
+        pubfile.read_to_string(&mut buf).await?;
+        let mut buf = Cursor::new(buf);
 
-            let mut buf = Vec::new();
-            let bufread = pubfile.read_buf(&mut buf).await;
-        }
+        // Private key reading
+        let mut pbuf = String::new();
+        privfile.read_to_string(&mut pbuf).await?;
+        let mut pbuf = Cursor::new(pbuf);
+
+        let key = rustls::PrivateKey(rustls_pemfile::pkcs8_private_keys(&mut pbuf)?.remove(0));
+
+        let certs : Vec<rustls::Certificate> = rustls_pemfile::certs(&mut buf)?
+            .into_iter()
+            .map(rustls::Certificate)
+            .collect();
+
+        (certs, key)
+    }
+    else {
+        log::warn!("Cannot load certificates. Generating self-signed certificates...");
+
+        let response = listener.input("Generating a self signed certificate. What are the domains on the certificate? Type the domains separated by a space.".to_string()).await?;
+
+        let cert = rcgen::generate_simple_self_signed(response.split(" ").map(|a| a.to_string()).collect::<Vec<String>>())?;
+        let key = rustls::PrivateKey(cert.serialize_private_key_der());
+
+        // Saving certificate
+        let c = cert.serialize_pem()?;
+        let k = cert.serialize_private_key_pem();
+
+        let mut f1 = File::create(&config.main_config.cert_path).await?;
+        let mut f2 = File::create(&config.main_config.private_key_path).await?;
+
+        f1.write_all(c.as_bytes()).await?;
+        f2.write_all(k.as_bytes()).await?;
+
+        (vec![rustls::Certificate(cert.serialize_der()?)], key)
+    };
+
+    let server_config = ServerConfig::with_single_cert(cert.0, cert.1)?;
+    let config = Arc::new(config);
+
+    // Feature Checks
+    let features = &config.main_config.features;
+
+    if features.contains("base") {
+        log::info!("Starting base node...");
+
+        let c = config.clone();
+
+        tokio::spawn(async move {
+            start_empty(c, server_config).await;
+        });
     }
 
     loop {}
@@ -212,7 +277,7 @@ async fn get_config() -> Result<Configuration, Box<dyn Error>> {
             // Write default config
             f.write_all(config::DEFAULT_CONFIG.as_bytes()).await?;
 
-            f
+            return Ok(config::Configuration::default());
         }
     };
 
@@ -246,6 +311,19 @@ impl TerminalListener {
         match n {
             ActionExecution::Select(s) => return Ok(s),
             ActionExecution::Input(_) => panic!(), // Shouldn't end up here
+        }
+    }
+    pub async fn input(&mut self, q: String) -> Result<String, Box<dyn Error>> {
+        // Send the password request
+        self.send
+            .send(InputChangeReq::Input(q, ui::InputType::Text))
+            .await?;
+
+        let n = self.recv.recv().await.unwrap();
+
+        match n {
+            ActionExecution::Input(s) => return Ok(s),
+            ActionExecution::Select(_) => panic!(), // Shouldn't end up here
         }
     }
     pub async fn password(&mut self, q: String) -> Result<String, Box<dyn Error>> {
