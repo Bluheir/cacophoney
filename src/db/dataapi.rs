047f170d3af1273d2094e8314b72d8822ffd8e7a
Bluheir <slimcognitodudesvids@gmail.com>
2022-10-03 10:31:47 +1100

    Renamed EmptyDB to EmptyDb

--- src/db/dataapi.rs
+++ src/db/dataapi.rs
@@ -5,13 +5,13 @@ use crate::data::{crypto::PubKey, SubAccount};
 #[async_trait]
 pub trait DbApi {
     // TODO add error type
-    async fn get_subaccounts(&mut self, key : &PubKey) -> Result<Vec<SubAccount>, ()>;
+    async fn get_subaccounts(&mut self, key: &PubKey) -> Result<Vec<SubAccount>, ()>;
 }
-pub struct EmptyDB {}
+pub struct EmptyDb {}
 
 #[async_trait]
-impl DbApi for EmptyDB {
-    async fn get_subaccounts(&mut self, _key : &PubKey) -> Result<Vec<SubAccount>, ()> {
+impl DbApi for EmptyDb {
+    async fn get_subaccounts(&mut self, _key: &PubKey) -> Result<Vec<SubAccount>, ()> {
         Err(())
     }
 }
