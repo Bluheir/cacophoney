e6a1eb728582d45b28e9cc85069722bbf62f5640
Bluheir <slimcognitodudesvids@gmail.com>
2022-10-03 10:31:15 +1100

    Formatted

--- src/data/message.rs
+++ src/data/message.rs
@@ -12,7 +12,7 @@ pub enum MessageHeader {
     /// A client identifies themself with a public key
     Identify = 1,
     /// An error
-    Error = 2
+    Error = 2,
 }
 /// Represents a generic message received by/sent to a client
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -30,27 +30,27 @@ pub struct Message {
 pub enum StreamIdentify {
     /// A normal stream with normal events
     Normal = 0,
-    Administration = 1
+    Administration = 1,
 }
 
 #[serde_as]
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Identifier {
-    /// Public keys and signatures 
-    pub identities : Vec<Identity>,
+    /// Public keys and signatures
+    pub identities: Vec<Identity>,
     /// Timestamp of the identify
     pub timestamp: DateTime<Utc>,
     /// The code that the client/server sent in an identify request
     #[serde_as(as = "[_; 32]")]
-    pub sig_msg : [u8; 32]
+    pub sig_msg: [u8; 32],
 }
 
 #[serde_as]
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Identity {
     /// Public key identifying as
-    pub key : PubKey,
+    pub key: PubKey,
     /// Digital signature of the timestamp
     #[serde_as(as = "[_; 64]")]
-    pub signature : [u8; 64]
+    pub signature: [u8; 64],
 }
