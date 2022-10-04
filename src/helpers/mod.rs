51edf095121757a189cfe8f21190e34a0c6efea7
Bluheir <slimcognitodudesvids@gmail.com>
2022-10-04 12:30:14 +1100

    Changed type parameter to &str

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,6 +1,8 @@
-pub mod file;
+pub use self::file::*;
+
+mod file;
 pub mod ip;
 
-pub fn hash_s(s: &String) -> [u8; 32] {
+pub fn hash_s(s: &str) -> [u8; 32] {
     blake3::hash(s.as_bytes()).as_bytes().clone()
 }
